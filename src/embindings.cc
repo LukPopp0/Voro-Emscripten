925e72d1ee71966a5d4a7a613b89e22b080f29ae
Lukas Popp <luk.popp0@gmail.com>
2023-07-08 01:56:12 -0400

    Add test in main

--- src/embindings.cc
+++ src/embindings.cc
@@ -56,7 +56,7 @@ public:
     std::vector<CellExport> cells;
     con.compute_cell_data(cells);
 
-    for(auto cell : cells) {
+    for (auto cell : cells) {
       std::cout << cell.particleID << " " << cell.nFaces << std::endl;
     }
   }
@@ -88,5 +88,49 @@ EMSCRIPTEN_BINDINGS(vorojs) {
 
 int main() {
   std::cout << "Voro++ loaded." << std::endl;
+  #ifndef EMSCRIPTEN
+  std::vector<float> points{0,
+                            -4.99995,
+                            0,
+                            -3.6899379709983346,
+                            -2.5739679448534636,
+                            -4.99995,
+                            3.576031624417964,
+                            -2.546234552807669,
+                            -4.99995,
+                            4.99995,
+                            -2.172426002791485,
+                            1.5834713245347982,
+                            0.037275242244040996,
+                            -2.071104653605047,
+                            4.99995,
+                            -4.99995,
+                            -2.182976164456667,
+                            1.665892796700179,
+                            -0.03727524224404482,
+                            2.071104653605047,
+                            -4.99995,
+                            4.99995,
+                            2.1829761644566674,
+                            -1.6658927967001833,
+                            3.689937970998336,
+                            2.5739679448534636,
+                            4.99995,
+                            -3.5760316244179706,
+                            2.546234552807671,
+                            4.99995,
+                            -4.99995,
+                            2.1724260027914855,
+                            -1.5834713245348024,
+                            1.974399169327309,
+                            4.99995,
+                            0.6252871464344901};
+
+  PointStorage p = PointStorage();
+  p.add_points(points);
+  Container c = Container();
+  c.compute_cells(p);
+  #endif
+
   return 0;
 }
